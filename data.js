24dc9c6b86eff0786e73d0bc51449712382dbb59
Berta López <bertalopez@MacBook-Pro-de-Berta.local>
2021-04-20 12:33:15 +0200

    lista con nombres, id y img

--- data.js
+++ data.js
@@ -1,5 +1,127 @@
-export const codersList [{
-    titles: ''
-}
-
+export const codersList [
+    {
+    name: 'Andrea',
+    id: 1
+    img: URL()
+    },  
+    {
+    name: 'Sergio',
+    id: 2
+    img: URL()
+    },
+    {
+    name: 'Joan',
+    id: 3
+    img: URL()
+    },
+    {
+    name: 'Andriu',
+    id: 4
+    img: URL()
+    },  
+    {
+    name: 'Gabi',
+    id: 5
+    img: URL()
+    },
+    {
+    name: 'Alexandra',
+    id: 6
+    img: URL()
+    },    
+    {
+    name: 'Jose Miguel',
+    id: 7
+    img: URL()
+    },  
+    {
+     name: 'Martín',
+    id: 8
+    img: URL()
+    },
+    {
+    name: 'Diego',
+    id: 9
+    img: URL()
+    },
+    {
+    name: 'Berta',
+    id: 10
+    img: URL()
+    },
+    {
+    name: 'Jael',
+    id: 11
+    img: URL()
+    },  
+    {
+    name: 'Anya',
+    id: 12
+    img: URL()
+    },
+    {
+    name: 'Leah',
+    id: 13
+    img: URL()
+    },
+    {
+    name: 'Thierno',
+    id: 14
+    img: URL()
+    },  
+    {
+    name: 'Cris',
+    id: 15
+    img: URL()
+    },
+    {
+    name: 'Solomon',
+    id: 16
+    img: URL()
+    },    
+    {
+    name: 'Snezhana',
+    id: 17
+    img: URL()
+    },  
+    {
+     name: 'El Hermoso',
+    id: 18
+    img: URL()
+    },
+    {
+    name: 'David',
+    id: 19
+    img: URL()
+    },
+    {
+    name: 'Abdulha',
+    id: 20
+    img: URL()
+    },
+    {
+    name: 'Andrés',
+    id: 21
+    img: URL()
+    }, 
+    {
+    name: 'Armando',
+    id: 22
+    img: URL()
+    },     
+    {
+    name: 'Gabriela',
+    id: 23
+    img: URL()
+    },    
+    {
+    name: 'Franco',
+    id: 24
+    img: URL()
+    },
+    {
+    name: 'Amr',
+    id: 25
+    img: URL()
+    }, 
 ]
