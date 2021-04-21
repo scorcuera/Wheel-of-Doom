919c9a05060ad1de864934bc36e37129074280e4
blaucomfuig <scorcuera6@gmail.com>
2021-04-21 15:50:01 +0200

    se añadió función que permite conectar boton matar coder con pintar pantalla coder seleccionado

--- random.js
+++ random.js
@@ -2,11 +2,33 @@ import { codersList } from "./data.js";
 
 let btMatar= document.querySelector(".btMatar")
 
-btMatar.addEventListener("click", () =>{
-  let random = Math.floor(Math.random() * codersList.length)
-  codersList.forEach(item)
-  if(codersList[random].id ==)
-})
+function oscurecerRandom(array){
+  btMatar.addEventListener("click", () =>{
+    let random = Math.floor(Math.random()* array.length)
+    let randomId = array[random].id
+
+    let codersPrinted = "";
+    array.forEach(item => {
+        if(item.id == randomId){
+        codersPrinted += `<div class="ctCoders" data-set="${item.id}"><h2>${item.name}</h2>
+    <img src="${item.img}" alt="freakCoder" class="freakCoder">
+    <img src="img/telonCoders.jpg" alt="stage" class="stage"></div>`
+      }
+    });
+    document.querySelector(".ctFichas").innerHTML = codersPrinted;
+  })
+}
+
+oscurecerRandom(codersList);
+
+
+
+
+
+
+
+
+
 
 
 
