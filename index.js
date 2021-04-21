a7f8350bf5f6987efcf79d87ccdd4394cb239086
Gabi Piñeiro <gabispineiro@gmail.com>
2021-04-21 13:48:05 +0200

    actualizacion

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 import { codersList } from "./data.js";
 
-
-for(let i=0; i<codersList.length; i++){
-  console.log(codersList[i].name)
-} 
-
-codersList.sort(function() { 
-  return Math.random((codersList.name) - 0.5)
-
-})
-  
+  let random = Math.floor(Math.random() * codersList.length)
+  console.log(codersList[random].name)
+ 
+ 
     
