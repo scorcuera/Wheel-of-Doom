3ece8ff56c5445d0cbaf6bd76d0e08f31af07d3f
MAlexGG <alexandragalarzag@gmail.com>
2021-04-22 16:01:22 +0200

    act

--- random.js
+++ random.js
@@ -1,27 +1,28 @@
 import { codersList } from "./data.js";
 
-let btMatar= document.querySelector(".btMatar")
+let btMatar = document.querySelector(".btMatar")
 
- btMatar.addEventListener("click", () =>{
-  let random = Math.floor(Math.random()* array.length)
-  let randomId = array[random].id;
-  
-  let contenedores = document.querySelectorAll(".ctCoders")
-  let randomDataset= contenedores[random].dataset.set
- 
-  array.map(item => {
-    
-      if(item.id == randomId){
-        let indice = array.indexOf(item)
-        array.splice(indice, 1)
-        console.log(array)
-        contenedores[randomId].classList.add('ctCodersSelected');
-      
-  };
+
+btMatar.addEventListener("click", () => {
+    let random = Math.floor(Math.random() * array.length)
+    let randomId = array[random].id;
+
+    let contenedores = document.querySelectorAll(".ctCoders")
+    let randomDataset = contenedores[random].dataset.set
+
+    array.map(item => {
+
+        if (item.id == randomId) {
+            let indice = array.indexOf(item)
+            array.splice(indice, 1)
+            console.log(array)
+            contenedores[randomId].classList.add('ctCodersSelected');
+        }
+    });
 });
- 
 
-  oscurecerRandom(codersList)
+
+oscurecerRandom(codersList);
 
 
 
