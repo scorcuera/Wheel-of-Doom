252e82d2da6ccb58c927450684e58bc9ca0f1538
Berta López <bertalopez@MacBook-Pro-de-Berta.local>
2021-04-23 10:11:54 +0200

    actualización

--- index2.js
+++ index2.js
@@ -1,17 +1,15 @@
 import { codersList } from "./data.js";
 
-console.log(codersList)
-
 //Funcion para traer la persona seleccionada y que le caiga el barril y la sangre
-function randomCodersPrinted (array) {
-    let randomCodersPrinted = "";
+function randomCoderSelectedPrinted (array) {
+    let randomCoderSelectedPrinted = "";
     array.forEach(randomId) => {
         randomCodersPrinted += `<div class="ctCodersBig" data-set="${randomId.id}"><h2>${randomId.name}</h2>
     <img src="${randomId.img}" alt="ctCodersBig" class="ctCodersBig">
     <img src="img/telonCoders.jpg" alt="stage" class="stage"></div>`
     });
 
-    document.querySelector(".ctFichas").innerHTML = randomCodersPrintedd;
+    document.querySelector(".ctCoders").innerHTML = randomCoderSelectedPrinted;
 };
 
 randomCodersPrinted (codersList);
